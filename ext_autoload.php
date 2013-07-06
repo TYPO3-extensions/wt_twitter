ec5ff26e9458e266a85786453b0847e0313620da
Nicole Cordes <cordes@cps-it.de>
2013-07-06 14:08:05 +0200

    [BUGFIX] Fatal error: Class 'Tx_WtTwitter_Utility_Compatibility' not found
    Class Tx_WtTwitter_Utility_Compatibility not found due to missing ext_autoload entry.
    
    Resolves: #49729
    Releases: 1.0
    Change-Id: I1fb758cf01023cfc4d74b6ce0d9c6e1616e3e47a
    Reviewed-on: https://review.typo3.org/22009
    Reviewed-by: Nicole Cordes
    Tested-by: Nicole Cordes

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,7 +1,9 @@
 <?php
 $extensionClassPath = t3lib_extMgm::extPath('wt_twitter') . 'Classes/';
+
 return array(
 	'tx_wttwitter_twitter_api' => $extensionClassPath . 'Twitter/Api.php',
+	'tx_wttwitter_utility_compatibility' => $extensionClassPath . 'Utility/Compatibility.php'
 );
 
 ?>
